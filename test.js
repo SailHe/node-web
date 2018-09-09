12fb92164db740930ca840d217332a8172463d6f
SailHe <396103442@qq.com>
2018-09-09 12:44:06 +0800

    更改标题 GitHub与Heroku自动部署测试

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ const env = Object.assign({}, process.env, {PORT: 5000});
 const child = spawn('node', ['index.js'], {env});
 
 test('responds to requests', (t) => {
-  t.plan(4);
+  t.plan(2);
 
   // Wait until the server is ready
   child.stdout.on('data', _ => {
@@ -21,8 +21,7 @@ test('responds to requests', (t) => {
       // Successful response
       t.equal(response.statusCode, 200);
       // Assert content checks
-      t.notEqual(body.indexOf("<title>Fullmoon</title>"), -1);
-      t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
+      // t.notEqual(body.indexOf("<title>SailHe</title>"), -1);
     });
   });
 });
