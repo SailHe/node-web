9afb8a6414b0a2f59c60c1729ab26bbe0f70d79f
SailHe <396103442@qq.com>
2018-12-11 23:11:28 +0800

    add aplayer module

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+// import 'APlayer/dist/APlayer.min.css';
+const APlayer  = require('aplayer')
+
+//const ap = new APlayer(options);
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
